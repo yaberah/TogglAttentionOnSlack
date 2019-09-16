06a450311087dac8bc8a7f412de25e25b67a8388
yaberah <yabe.masato.ly@alumni.tsukuba.ac.jp>
2019-09-16 15:03:08 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const axios = require('axios');
+const fs = require('fs');
+
+// For local
+try {
+  fs.statSync('.env');
+  require('dotenv').config();
+} catch { }
+
+const toggl_api_key   = process.env.TOGGL_API_KEY;
+const user_agent      = process.env.USER_AGENT;
+const workspace_id    = process.env.WORKSPACE_ID;
+const slack_api_url   = process.env.SLACK_API_URL;
+const user_list       = process.env.USER_LIST;
+
+const users = user_list.split('$').map((user) => {
+  return {
+    name: user.split(',')[0],
+    toggl_id: user.split(',')[1],
+    slack_id: user.split(',')[2],
+  }
+});
+
+const toggl_base_url  = "https://toggl.com/reports/api/v2/weekly";
+
+const getYesterday = () => {
+  const today = new Date();
+  const yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1)
+  const YYYY  = yesterday.getFullYear();
+  const MM    = (yesterday.getMonth() + 1).toString().padStart(2, '0');
+  const DD    = yesterday.getDate().toString().padStart(2, '0');
+  const day   = yesterday.getDay();
+
+  return {
+    yesterday: YYYY + "-" + MM + "-" + DD,
+    day : day
+  }
+}
+
+const yesterday = getYesterday().yesterday;
+const day = getYesterday().day;
+
+exports.handler = (event, context) => {
+  users.forEach((user) => {
+    const user_ids = user.toggl_id;
+    const params = { user_agent, workspace_id, yesterday, user_ids };
+    const toggl_api_url = toggl_base_url + `?user_agent=${params.user_agent}&workspace_id=${params.workspace_id}&since=${params.yesterday}&user_ids=${params.user_ids}`;
+
+    axios
+    .get( toggl_api_url , { headers: { Authorization: `Basic ${toggl_api_key}`}})
+    .then((response) => {
+      const total = response.data.week_totals[Array.length - 1] / 3600 / 1000;
+      const totalHour = Math.floor(total);
+      const totalMin  = (total - totalHour) * 60;
+      const totalTime = totalHour + ":" + Math.floor(totalMin).toString().padStart(2, '0');;
+
+      if ( total < 7 && day !== 0 && day !== 6 ) {
+          console.log(total, yesterday, day);
+          console.log(`<@${user.slack_id}|${user.name}> Yesterday(${yesterday}), Your Toggl duration time was ${totalTime}.`);
+
+          axios
+          .post( slack_api_url ,{
+            text: `<@${user.slack_id}|${user.name}> Yesterday(${yesterday}), Your Toggl duration was ${totalTime}. \n昨日のTogglの入力時間が7時間を下回っています。`
+          })
+          .catch((error)=>{
+            console.log("Failure posting to slack.");
+          })
+        }
+
+    })
+    .catch((error) => {
+      console.error(error.response.status);
+      console.error(error.response.statusText);
+      console.error(error.response.config);
+      console.error(error.response.data);
+      console.error("-------");
+    });
+  })
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+var lambda = require('./index.js');
+
+var event = null;
+var context = null;
+
+lambda.handler(event ,context);
