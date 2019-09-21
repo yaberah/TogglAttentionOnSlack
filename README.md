1ddeb8a194cd9eb78893ee4019312a154132345d
yaberah <yabe.masato.ly@alumni.tsukuba.ac.jp>
2019-09-21 12:16:49 +0900

    NEW images repository

--- README.md
+++
@@ -1,2 +0,0 @@
-# TogglAttentionOnSlack
-Slack bot which send message if user's Toggl duration is shorter than 7 hours.
--- test.js
+++
@@ -1,6 +0,0 @@
-var lambda = require('./index.js');
-
-var event = null;
-var context = null;
-
-lambda.handler(event ,context);
